4803b450498c61746fe9647889911968e3ecc032
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2021-10-01 02:36:11 +0300

    fin

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict"
+
+const menuHeader = document.querySelector('.header__menu');
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+    const menuHeader = document.querySelector('.header__menu');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuHeader.classList.toggle('_active');
+    })
+}
+
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLinks => {
+        menuLinks.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuHeader.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+
